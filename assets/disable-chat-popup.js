ee04f7430aebae750afd7a07a2fd39c2db8b876f
safedd <130098372+safedd@users.noreply.github.com>
2023-04-26 17:05:14 +0530

    Create disable-chat-popup.js

---
+++ assets/disable-chat-popup.js
@@ -0,0 +1,34 @@
+function toggleDivVisibility(targetDiv, toggleButton) {
+  targetDiv.style.display = 'none';
+
+  toggleButton.addEventListener('click', () => {
+    if (targetDiv.style.display === 'none') {
+      targetDiv.style.display = 'block';
+    } else {
+      targetDiv.style.display = 'none';
+    }
+  });
+}
+
+function initObserver() {
+  const targetDivSelector = 'div.sc-gsDKAQ.bRVohJ';
+  const toggleButtonSelector = 'div.sc-hKwDye.hWATBB > div > div';
+
+  const observer = new MutationObserver((mutationsList, observer) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        const targetDiv = document.querySelector(targetDivSelector);
+        const toggleButton = document.querySelector(toggleButtonSelector);
+
+        if (targetDiv && toggleButton) {
+          toggleDivVisibility(targetDiv, toggleButton);
+          observer.disconnect(); // Disconnect the observer once the elements are found and processed
+        }
+      }
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+}
+
+document.addEventListener('DOMContentLoaded', initObserver);
